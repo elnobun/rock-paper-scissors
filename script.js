2224940515cadefa75b4b1d94e38a6eefab36fa7
elnobun <elnobun@gmail.com>
2021-01-24 16:34:23 +0000

    Implemented playRounds logic

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+// Create variable with an array of rock, paper, scissors
+const choices = ["rock", "paper", "scissors"];
+
 // Create function call `computerPlay()`
 function computerPlay() {
-    // Create variable with an array of rock, paper, scissors
-    const choices = ["rock", "paper", "scissors"];
-    // Store random choice in a new variable
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    // Return the random choice. Ensure the first letter is uppercase.
-    return randomChoice.charAt(0).toUpperCase() + randomChoice.slice(1);
+  // Store random choice in a new variable
+  const randomChoice = [...choices];
+  return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 }
-console.log(computerPlay());
+
+// Create function call `playRounds()`
+function playRounds(playerSelection, computerSelection) {
+  const playerChoice = playerSelection.toLowerCase();
+  const computerChoice = computerSelection.toLowerCase();
+
+  if (playerChoice === "rock" && computerChoice === "scissors") {
+    return `You Win! Rock beats Scissors`;
+  } else if (playerChoice === "rock" && computerChoice === "paper") {
+    return `You Lose! Paper beats Rock`;
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    return `You Win! Scissors beats Paper`;
+  } else if (playerChoice === "scissors" && computerChoice === "rock") {
+    return `You Lose! Rock beats Scissors`;
+  } else {
+    return "It is a draw";
+  }
+}
+
+console.log(playRounds("rock", computerPlay()));
